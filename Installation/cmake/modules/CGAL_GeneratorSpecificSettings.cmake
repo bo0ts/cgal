59d4bf6fd5bd50ce6429ad87d7b42c61bd93ebb6
Philipp Möller <bootsarehax@gmail.com>
2015-06-24 17:49:57 +0200

    HAS_CFG_INTDIR is only used in src/CMakeLists.txt
    Use it directly in the condition instead of polluting the cache.

--- Installation/cmake/modules/CGAL_GeneratorSpecificSettings.cmake
+++ Installation/cmake/modules/CGAL_GeneratorSpecificSettings.cmake
@@ -8,9 +8,4 @@ if ( NOT CGAL_GENERATOR_SPECIFIC_SETTINGS_FILE_INCLUDED )
     set(CGAL_AUTO_LINK_ENABLED TRUE)
   endif()
 
-  if ( NOT "${CMAKE_CFG_INTDIR}" STREQUAL "." )
-    set(HAS_CFG_INTDIR TRUE CACHE INTERNAL "Generator uses intermediate configuration directory" )
-    message( STATUS "Generator uses intermediate configuration directory: ${CMAKE_CFG_INTDIR}" )
-  endif()
-
 endif()
