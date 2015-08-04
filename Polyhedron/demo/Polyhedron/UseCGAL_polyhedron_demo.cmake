252a3ce35ede9dba2f1cb6182f0a4f949b793ea5
Philipp Möller <bootsarehax@gmail.com>
2015-08-04 14:43:36 +0200

    Include the targets via the Config.cmake

--- Polyhedron/demo/Polyhedron/UseCGAL_polyhedron_demo.cmake
+++ Polyhedron/demo/Polyhedron/UseCGAL_polyhedron_demo.cmake
@@ -5,7 +5,6 @@ list(INSERT CMAKE_MODULE_PATH 0 "${CGAL_POLYHEDRON_DEMO_CMAKE_MODULE_PATH}")
 message(STATUS "Now CMAKE_MODULE_PATH is: ${CMAKE_MODULE_PATH}")
 
 include( polyhedron_demo_macros )
-include( polyhedron_demo_targets )
 
 
 include_directories( ${CGAL_POLYHEDRON_DEMO_HEADERS_DIRS} )
