d897e826b8d5833a738aef3d32846186b3baf994
Philipp Möller <bootsarehax@gmail.com>
2015-08-04 14:48:43 +0200

    Cut down the Use file

--- Polyhedron/demo/Polyhedron/UseCGAL_polyhedron_demo.cmake
+++ Polyhedron/demo/Polyhedron/UseCGAL_polyhedron_demo.cmake
@@ -1,11 +1,2 @@
-message(STATUS "Using CGAL_polyhedron_demo from: ${CGAL_POLYHEDRON_DEMO_DIR}")
-
-list(INSERT CMAKE_MODULE_PATH 0 "${CGAL_POLYHEDRON_DEMO_CMAKE_MODULE_PATH}")
-
-message(STATUS "Now CMAKE_MODULE_PATH is: ${CMAKE_MODULE_PATH}")
-
-include( polyhedron_demo_macros )
-
-
-include_directories( ${CGAL_POLYHEDRON_DEMO_HEADERS_DIRS} )
+include_directories(${CGAL_POLYHEDRON_DEMO_HEADERS_DIRS})
 add_definitions(${CGAL_POLYHEDRON_DEMO_DEFINITIONS})
